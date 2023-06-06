9227f8ce699a1fc3df5fef12739a4fdc005ccfd4
Vignnesh <ravin.vignnesh@gmail.com>
2023-06-06 11:44:51 +0800

    Search function (via prompt/tag)

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import { useState, useEffect } from "react";
 
@@ -20,25 +20,55 @@ const PromptCardList = ({ data, handleTagClick }) => {
 
 const Feed = () => {
 	const [searchText, setSearchText] = useState("");
+	const [searchTimeout, setSearchTimeout] = useState(null);
+	const [searchedResults, setSearchedResults] = useState([]);
 	const [posts, setPosts] = useState([]);
-
-	const handleSearchChange = (e) => {};
 
 	useEffect(() => {
 		const fetchPosts = async () => {
-			const response = await fetch("/api/prompt", { 
-        method: 'GET', 
-        headers: {
-          "Cache-Control": "no-store", // Disables caching
-        }
-      });
+			const response = await fetch("/api/prompt", {
+				method: "GET",
+				headers: {
+					"Cache-Control": "no-store", // Disables caching
+				},
+			});
 			const data = await response.json();
-      console.log(data);
+			console.log(data);
 			setPosts(data);
 		};
 
 		fetchPosts();
 	}, []);
+
+	const filterPrompts = (searchText) => {
+		const regex = new RegExp(searchText, "i"); // 'i' flag for case-insensitive
+		return posts.filter(
+			(item) =>
+				regex.test(item.creator.username) ||
+				regex.test(item.tag) ||
+				regex.test(item.prompt)
+		);
+	};
+
+	const handleSearchChange = (e) => {
+		clearTimeout(searchTimeout);
+		setSearchText(e.target.value);
+
+		// Debounce method
+		setSearchTimeout(() => {
+			setTimeout(() => {
+				const searchResult = filterPrompts(e.target.value);
+				setSearchedResults(searchResult);
+			}, 500);
+		});
+	};
+
+	const handleTagClick = (tagName) => {
+		setSearchText(tagName);
+
+		const searchResult = filterPrompts(tagName);
+		setSearchedResults(searchResult);
+	};
 
 	return (
 		<section className="feed">
@@ -54,8 +84,8 @@ const Feed = () => {
 			</form>
 
 			<PromptCardList
-				data={posts}
-				handleTagClick={() => {}}
+				data={ searchedResults ?? posts }
+				handleTagClick={handleTagClick}
 			/>
 		</section>
 	);
